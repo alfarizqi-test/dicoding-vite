f452ed02a81ff2892281dfa9d1cb3083775cd2da
Architect <alfarizqi.stura@gmail.com>
2026-05-10 16:04:04 +0700

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/dicoding-vite/sw.js', { scope: '/dicoding-vite/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
